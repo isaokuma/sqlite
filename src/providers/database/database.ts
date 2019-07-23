1b5461d460f13e639e5302e0e83f133750e64f1f
isaokuma <38734733+isaokuma@users.noreply.github.com>
2019-07-22 22:25:33 -0300

    Update database.ts

--- src/providers/database/database.ts
+++ src/providers/database/database.ts
@@ -6,9 +6,6 @@ export class DatabaseProvider {
  
   constructor(private sqlite: SQLite) { }
  
-  /**
-   * Cria um banco caso não exista ou pega um banco existente com o nome no parametro
-   */
   public getDB() {
     return this.sqlite.create({
       name: 'products.db',
@@ -16,9 +13,6 @@ export class DatabaseProvider {
     });
   }
  
-  /**
-   * Cria a estrutura inicial do banco de dados
-   */
   public createDatabase() {
     return this.getDB()
       .then((db: SQLiteObject) => {
@@ -33,12 +27,7 @@ export class DatabaseProvider {
       .catch(e => console.log(e));
   }
  
-  /**
-   * Criando as tabelas no banco de dados
-   * @param db
-   */
   private createTables(db: SQLiteObject) {
-    // Criando as tabelas
     db.sqlBatch([
       ['CREATE TABLE IF NOT EXISTS categories (id integer primary key AUTOINCREMENT NOT NULL, name TEXT)'],
       ['CREATE TABLE IF NOT EXISTS products (id integer primary key AUTOINCREMENT NOT NULL, name TEXT, price REAL, duedate DATE, active integer, category_id integer, FOREIGN KEY(category_id) REFERENCES categories(id))']
@@ -47,14 +36,9 @@ export class DatabaseProvider {
       .catch(e => console.error('Erro ao criar as tabelas', e));
   }
  
-  /**
-   * Incluindo os dados padrões
-   * @param db
-   */
   private insertDefaultItems(db: SQLiteObject) {
     db.executeSql('select COUNT(id) as qtd from categories', [])
     .then((data: any) => {
-      //Se não existe nenhum registro
       if (data.rows.item(0).qtd == 0) {
  
         // Criando as tabelas
